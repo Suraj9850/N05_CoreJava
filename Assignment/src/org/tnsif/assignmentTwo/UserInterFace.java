e85f13d87c4ef4647b8f8373115762b65d64d20b
Suraj Vishwakarma <Surajvishwakarma9850@gmail.com>
2023-10-09 13:45:53 +0530

    commit

---
+++ Assignment/src/org/tnsif/assignmentTwo/UserInterFace.java
@@ -0,0 +1,35 @@
+package org.tnsif.assignmentTwo;
+
+import java.util.Scanner;
+
+public class UserInterFace {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Products p=new Products();
+		Scanner sc=new Scanner(System.in);
+		while(true)
+		{	
+			System.out.println("1.Add\n2.Display\n3.Exit\n");
+			System.out.println("\nEnter your choice:");
+			int ch=sc.nextInt();
+			switch (ch) {
+			case 1:
+					sc.nextLine();
+					System.out.println("Enter the product:");
+					String productName=sc.nextLine();
+					p.addProductToList(productName);
+				break;
+			case 2:
+					p.sortProductList();
+					break;
+			case 3:
+					System.out.println("thank you");
+					System.exit(0);
+			default:
+				break;
+			}
+		}
+	}
+
+}
